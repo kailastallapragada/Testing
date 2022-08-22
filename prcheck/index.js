cfb2d830727f5e090ff98a51ee2ca3d28fae534c
kailas-tallapragada <kailas.tallapragada@freshworks.com>
2022-08-22 21:52:37 +0530

    CHANGES

--- prcheck/index.js
+++ prcheck/index.js
@@ -3,11 +3,14 @@ const github = require('@actions/github');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const REPOSITORY = core.getInput('REPOSITORY');
+    const PR = core.getInput('PR');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
+    await octokit.request(`POST /repos/${REPOSITORY}/issues/${PR}/labels`)
     await octokit.issues.createLabel({
         ...context.repo,
         issue_number: pull_request.number,
