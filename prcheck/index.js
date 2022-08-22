fa35700da745b918d1cf310c2b3ba4fd0ebeab4c
kailas-tallapragada <kailas.tallapragada@freshworks.com>
2022-08-22 22:41:22 +0530

    CHANGES

--- prcheck/index.js
+++ prcheck/index.js
@@ -3,17 +3,12 @@ const github = require('@actions/github');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const REPOSITORY = core.getInput('REPOSITORY');
-    const PR = core.getInput('PR');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    console.log("repo:", context.repo);
-    console.log("number:", pull_request.number);
-
-    await octokit.request(`POST /repos/${REPOSITORY}/issues/${PR}/labels`, {
+    await octokit.request(`POST /repos/${context.repo.owner}/${context.repo.repo}/issues/${pull_request.number}/labels`, {
         labels: ["ForceMerged"]
     });
 }
