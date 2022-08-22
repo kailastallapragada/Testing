54272abbb6c73efcab3ccc919eb1cfd25bc41821
kailas-tallapragada <kailas.tallapragada@freshworks.com>
2022-08-22 12:35:09 +0530

    Githubworkflows (#14)
    * Adding sample workflow.
    
    * Adding sample workflow.
    
    * Adding sample workflow.
    
    * Adding sample workflow.
    
    * Adding sample workflow.
    
    * Adding Github token.
    
    * Add review output
    
    * Add review output
    
    * Add review output
    
    * Add review output
    
    * Adding nodejs code
    
    * Checkout
    
    * Adding action

--- prcheck/index.js
+++ prcheck/index.js
@@ -1,5 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-    console.log("Hello World");
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+
+    await octokit.issues.createLabel({
+        ...context.repo,
+        issue_number: pull_request.number,
+        labels: ["ForceMerged"]
+    });
 }
 
 run();
